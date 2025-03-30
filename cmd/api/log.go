d3fed6494b461157d7ad198bd154ad4b36929c27
Kobie Danquah <devdanquah@gmail.com>
2025-03-30 14:39:56 +0000

    add logging setup and example environment configuration

--- cmd/api/log.go
+++ cmd/api/log.go
@@ -1,6 +1,28 @@
 package main
 
+import (
+	"io"
+	"log/slog"
+	"os"
+)
 
-func init(){
-	// TODO: Add default logger
+func init() {
+
+	var writer io.Writer
+	var err error
+	if os.Getenv("ENV") != "dev" {
+		writer, err = os.OpenFile("split.log", os.O_CREATE|os.O_RDWR, 0755)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		writer = os.Stdout
+	}
+
+	logger := slog.New(slog.NewJSONHandler(writer, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+	}))
+
+	slog.SetDefault(logger)
 }
